877d7ed80de1ca309a7c39da8033a67c17df3dbf
scrubbyJoe <paytonheckman@gmail.com>
2025-11-29 13:12:41 -0500

    started adding settings to database

--- app/src/main/java/com/example/pocketegbert/GameScreen.java
+++ app/src/main/java/com/example/pocketegbert/GameScreen.java
@@ -284,5 +284,4 @@ public class GameScreen extends Fragment {
             }
         });
     }
-
 }
--- app/src/main/java/com/example/pocketegbert/gameData.java
+++ app/src/main/java/com/example/pocketegbert/gameData.java
@@ -10,12 +10,19 @@ public class gameData {
     int userId;
     int score;
     int happiness;
+    /*
+    int difficulty;
+    int background;
+    */
 
-
-    public gameData(int score, int happiness, int userId){
+    public gameData(int score, int happiness, int userId, int difficulty, int background){
         this.score=score;
         this.happiness=happiness;
         this.userId=userId;
+        /*
+        this.difficulty=difficulty;
+        this.background=background;
+         */
     }
 
     //getters and setters
@@ -31,8 +38,25 @@ public class gameData {
         this.happiness=happiness;
     }
 
+    /*
     public int getHappiness(){
         return happiness;
     }
 
+    public void setDifficulty(int difficulty){
+        this.difficulty=difficulty;
+    }
+
+    public int getDifficulty(){
+        return difficulty;
+    }
+
+    public void setBackground(int background){
+        this.background=background;
+    }
+
+    public int getBackground(){
+        return background;
+    }
+     */
 }
